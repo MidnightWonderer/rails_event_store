9eb3569d871df1d5326b4a0738cdaaadde95f0f6
MidnightWonderer <midnight_w@gmx.com>
2018-03-07 17:59:40 +0700

    use the current version for the migration generators

--- rails_event_store_active_record/spec/migration_generator_spec.rb
+++ rails_event_store_active_record/spec/migration_generator_spec.rb
@@ -14,7 +14,7 @@ module RailsEventStoreActiveRecord
     specify do
       FakeFS.with_fresh do
         FakeFS::FileSystem.clone(File.expand_path('../../', __FILE__))
-        stub_const("Rails::VERSION::STRING", "4.2.8")
+        stub_const("ActiveRecord::VERSION::STRING", "4.2.8")
 
         generator = MigrationGenerator.new
         allow(Time).to receive(:now).and_return(Time.new(2016,8,9,22,22,22))
@@ -27,13 +27,15 @@ module RailsEventStoreActiveRecord
     specify do
       FakeFS.with_fresh do
         FakeFS::FileSystem.clone(File.expand_path('../../', __FILE__))
-        stub_const("Rails::VERSION::STRING", "5.0.0")
+        stub_const("ActiveRecord::VERSION::STRING", "5.0.0")
 
         generator = MigrationGenerator.new
         allow(Time).to receive(:now).and_return(Time.new(2016,8,9,22,22,22))
         generator.create_migration
 
-        expect(File.read("db/migrate/20160809222222_create_event_store_events.rb")).to match(/ActiveRecord::Migration\[4\.2\]$/)
+        active_record_version = Gem::Version.new(ActiveRecord::VERSION::STRING)
+        quoted_active_record_version = Regexp.escape(active_record_version.to_s.sub(/\A(\d+\.\d+)\.\d+\z/, '\1'))
+        expect(File.read("db/migrate/20160809222222_create_event_store_events.rb")).to match(/ActiveRecord::Migration\[#{quoted_active_record_version}]$/)
       end
     end
   end
--- rails_event_store_active_record/spec/v1_v2_migration_generator_spec.rb
+++ rails_event_store_active_record/spec/v1_v2_migration_generator_spec.rb
@@ -14,7 +14,7 @@ module RailsEventStoreActiveRecord
     specify do
       FakeFS.with_fresh do
         FakeFS::FileSystem.clone(File.expand_path('../../', __FILE__))
-        stub_const("Rails::VERSION::STRING", "4.2.8")
+        stub_const("ActiveRecord::VERSION::STRING", "4.2.8")
 
         generator = V1V2MigrationGenerator.new
         allow(Time).to receive(:now).and_return(Time.new(2016,8,9,22,22,22))
@@ -27,13 +27,15 @@ module RailsEventStoreActiveRecord
     specify do
       FakeFS.with_fresh do
         FakeFS::FileSystem.clone(File.expand_path('../../', __FILE__))
-        stub_const("Rails::VERSION::STRING", "5.0.0")
+        stub_const("ActiveRecord::VERSION::STRING", "5.0.0")
 
         generator = V1V2MigrationGenerator.new
         allow(Time).to receive(:now).and_return(Time.new(2016,8,9,22,22,22))
         generator.create_migration
 
-        expect(File.read("db/migrate/20160809222222_migrate_res_schema_v1_to_v2.rb")).to match(/ActiveRecord::Migration\[4\.2\]$/)
+        active_record_version = Gem::Version.new(ActiveRecord::VERSION::STRING)
+        quoted_active_record_version = Regexp.escape(active_record_version.to_s.sub(/\A(\d+\.\d+)\.\d+\z/, '\1'))
+        expect(File.read("db/migrate/20160809222222_migrate_res_schema_v1_to_v2.rb")).to match(/ActiveRecord::Migration\[#{quoted_active_record_version}]$/)
       end
     end
   end
